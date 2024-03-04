7526bb1737f56d907531532696d0c93c5a817975
Bilol Sanatillaev <sanatillayevbilol@gmail.com>
2024-03-04 09:23:45 +0500

    async stream

---
+++ SwiftConcurrencyBootcamp/AsyncStream.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncStream.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Bilol Sanatillayev on 29/02/24.
+//
+
+import SwiftUI
+
+struct AsyncStream: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncStream()
+}
